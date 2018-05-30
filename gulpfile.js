d143610d06d2ecca28931763c6d5c70b107f4170
Jackie Lealess <jlealess@gmail.com>
2018-05-30 12:55:55 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('js', () => {
+    gulp.src('./dev/scripts/main.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('./public/scripts'))
+});
+
+gulp.task('styles', () => {
+   return gulp.src('./dev/styles/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./public/styles')) 
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./dev/styles/**/*.scss', ['styles']);
+    gulp.watch('./dev/scripts/main.js', ['js']);
+});
+
+gulp.task('default', ['styles', 'js', 'watch']);
+
